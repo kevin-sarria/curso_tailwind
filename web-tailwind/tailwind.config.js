fdd4bdf43b0013b8d98e6e5f569ea9fc1158003e
Kevin Sarria <reysabueso256@gmail.com>
2022-09-23 10:50:13 -0500

    avances

---
+++ web-tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    theme: {
+        extend: {
+            backgroundImage: theme => ({
+                'body-pattern': "url('../img/pattern.png')"
+            })
+        }
+    },
+    variants: {
+
+    }
+
+}
